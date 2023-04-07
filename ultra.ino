cf978d54ad568f634b05202e556b08e6b6ebc00c
pieteroyens <oyenspieter@gmail.com>
2023-04-07 12:07:31 +0200

    Add files via upload

---
+++ ultra.ino
@@ -0,0 +1,37 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN  9
+#define ECHO_PIN     10
+#define MAX_DISTANCE 400 // Maximum distance we want to measure (in centimeters).
+#define MIN_DISTANCE_TO_VACCUM_WALL 20
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(50);                    // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+  int distance = sonar.ping_cm(); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  int distance_median = sonar.convert_cm(sonar.ping_median(10));
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print("  ");
+  Serial.print(distance_median);
+  Serial.print("cm");
+  Serial.print(" In range: ");
+  Serial.print(isPingPongInRange(distance_median));
+  Serial.println();
+}
+
+bool isPingPongInRange(int current_distance) {
+  if (current_distance < MIN_DISTANCE_TO_VACCUM_WALL){
+    return true;
+  }
+
+  return false;
+}

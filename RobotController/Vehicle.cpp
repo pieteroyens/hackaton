f06afbfdaabc123e276981b511f4e7d621133e19
ru1t3rl <lucaruiters@hotmail.nl>
2023-04-07 12:28:18 +0200

    Fixed rotating right and left

--- RobotController/Vehicle.cpp
+++ RobotController/Vehicle.cpp
@@ -10,9 +10,9 @@
 ///////////////////////////
 
 const float degrees_to_milliseconds = .2;
+const float multiplier = 2.0f;
 
-Vehicle::Vehicle(Motor leftMotor, Motor rightMotor, int speed)
-{
+Vehicle::Vehicle(Motor leftMotor, Motor rightMotor, int speed) {
   _leftMotor = leftMotor;
   _rightMotor = rightMotor;
 
@@ -22,8 +22,7 @@ Vehicle::Vehicle(Motor leftMotor, Motor rightMotor, int speed)
   _isSetup = false;
 }
 
-void Vehicle::setup()
-{
+void Vehicle::setup() {
   if (_isSetup)
     return;
 
@@ -35,8 +34,7 @@ void Vehicle::setup()
   Serial.println("Vehicle is setup");
 }
 
-void Vehicle::forward()
-{
+void Vehicle::forward() {
   if (!_isSetup)
     setup();
 
@@ -44,8 +42,7 @@ void Vehicle::forward()
   _rightMotor.forward();
 }
 
-void Vehicle::forward(int milliseconds)
-{
+void Vehicle::forward(int milliseconds) {
   if (!_isSetup)
     setup();
 
@@ -55,8 +52,7 @@ void Vehicle::forward(int milliseconds)
   stop();
 }
 
-void Vehicle::backward()
-{
+void Vehicle::backward() {
   if (!_isSetup)
     setup();
 
@@ -64,8 +60,7 @@ void Vehicle::backward()
   _rightMotor.backward();
 }
 
-void Vehicle::backward(int milliseconds)
-{
+void Vehicle::backward(int milliseconds) {
   if (!_isSetup)
     setup();
 
@@ -74,24 +69,20 @@ void Vehicle::backward(int milliseconds)
   stop();
 }
 
-void Vehicle::left()
-{
+void Vehicle::left() {
   if (!_isSetup)
     setup();
 
   _leftMotor.backward();
   _rightMotor.forward();
 
-  Serial.println("Left 90 degrees");
-
-  delay(450);
+  delay((90 / (_leftMotor.getPower() / (255 * 1.0f))) * multiplier);
 
   stop();
 }
 
-void Vehicle::leftAngle(int angle)
-{
-  if(!_isSetup)
+void Vehicle::leftAngle(int angle) {
+  if (!_isSetup)
     setup();
 
   angle %= 360;
@@ -99,14 +90,25 @@ void Vehicle::leftAngle(int angle)
   _leftMotor.backward();
   _rightMotor.forward();
 
-  delay(angle * degrees_to_milliseconds);
+  delay((angle / (_leftMotor.getPower() / (255 * 1.0f))) * multiplier);
 
   stop();
 }
 
-void Vehicle::leftMilliseconds(int milliseconds)
-{
-  if(!_isSetup)
+void Vehicle::leftAngle(int angle, int power) {
+  int backupPower = _leftMotor.getPower();
+  
+  _leftMotor.setPower(power);
+  _rightMotor.setPower(power);
+
+  leftAngle(angle);  
+
+  _leftMotor.setPower(backupPower);
+  _rightMotor.setPower(backupPower);  
+}
+
+void Vehicle::leftMilliseconds(int milliseconds) {
+  if (!_isSetup)
     setup();
 
   _leftMotor.backward();
@@ -117,22 +119,20 @@ void Vehicle::leftMilliseconds(int milliseconds)
   stop();
 }
 
-void Vehicle::right()
-{
+void Vehicle::right() {
   if (!_isSetup)
     setup();
 
   _leftMotor.forward();
   _rightMotor.backward();
 
-  delay(450);
+  delay((90 / (_leftMotor.getPower() / (255 * 1.0f))) * multiplier);
 
   stop();
 }
 
-void Vehicle::rightAngle(int angle)
-{
-  if(!_isSetup)
+void Vehicle::rightAngle(int angle) {
+  if (!_isSetup)
     setup();
 
   angle %= 360;
@@ -140,14 +140,25 @@ void Vehicle::rightAngle(int angle)
   _leftMotor.forward();
   _rightMotor.backward();
 
-  delay(angle * degrees_to_milliseconds);
+  delay((angle / (_leftMotor.getPower() / (255 * 1.0f))) * multiplier);
 
   stop();
 }
 
-void Vehicle::rightMilliseconds(int milliseconds)
-{
-  if(!_isSetup)
+void Vehicle::rightAngle(int angle, int power) {
+  int backupPower = _leftMotor.getPower();
+  
+  _leftMotor.setPower(power);
+  _rightMotor.setPower(power);
+
+  rightAngle(angle);  
+
+  _leftMotor.setPower(backupPower);
+  _rightMotor.setPower(backupPower);  
+}
+
+void Vehicle::rightMilliseconds(int milliseconds) {
+  if (!_isSetup)
     setup();
 
   _leftMotor.forward();
@@ -158,17 +169,15 @@ void Vehicle::rightMilliseconds(int milliseconds)
   stop();
 }
 
-void Vehicle::stop()
-{
-  if(!_isSetup)
+void Vehicle::stop() {
+  if (!_isSetup)
     setup();
 
   _leftMotor.stop();
   _rightMotor.stop();
 }
 
-void Vehicle::setPower(int power)
-{
+void Vehicle::setPower(int power) {
   _leftMotor.setPower(power);
   _rightMotor.setPower(power);
 }
